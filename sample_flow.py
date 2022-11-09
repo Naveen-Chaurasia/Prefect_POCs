c6e4b6457e9420a3879c9903ab83b7d1853f124e
Naveen <naveen.chaurasia861@gmail.com>
2022-11-09 09:28:13 +0530

    added task for header of csv file in the flow

--- sample_flow.py
+++ sample_flow.py
@@ -1,6 +1,8 @@
 import requests
 from prefect import flow, task
 import json
+import csv
+
 @task
 def call_api(url):
     access_token = '1YTWX5GJMQME16GGFK5G6N5MB96M'
@@ -15,14 +17,25 @@ def parse_fact(response):
     return fact
     
 @task
+def add_header_of_csv(results): 
+    fields = ['id', 'name', 'category', 'sector','constituent_gases'] 
+    filename = "climateiq.csv"
+    # writing to csv file 
+    with open(filename, 'w') as csvfile: 
+    # creating a csv writer object 
+     csvwriter = csv.writer(csvfile) 
+     # writing the fields 
+     csvwriter.writerow(fields) 
+    return results
+    
+    
+@task
 def split_result(results):
     #datadict = json.load(results)
     #datatict is a list of dict.
     datadict=results
     print("*******************")
     print(type(datadict))
-    
-    
     ids=[]
     for item in datadict:
      print("*******************")
@@ -36,7 +49,8 @@ def split_result(results):
 def sample_flow(url):
     fact_json = call_api(url)
     fact_text = parse_fact(fact_json)
-    fact_id=split_result(fact_text)
+    results=add_header_of_csv(fact_text)
+    fact_id=split_result(results)
     print("----------------------")
     print(fact_id)
     return fact_id
